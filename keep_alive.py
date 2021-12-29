a1e3fb4045245a58128632c1b28f3990b21e19a3
Robotz89 <96810015+Robotz89@users.noreply.github.com>
2021-12-28 23:33:01 -0600

    keep_alive.py
    This is the code to run a web server for the bot! Follow the instructions, and your bot will never go offline.

---
+++ keep_alive.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I am now up and running as Galaxy Counting!"
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+#this is optional, but if you want to, you can use this web server code to keep the bot always on
+#all you have to do is copy the link up at the top when you run it, and copy it
+#then go to this website: https://uptimerobot.com/
+#create an account and add a monitor
+#select HTTPS, and put in the link where it says: https://
+#put a name in the section that is labeled: Friendly Name
+#click Create Monitor, then hit it one more time
+#you are done, and the bot will now be always running
