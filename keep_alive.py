22c97050181a8f3b1db3dbc40c2036eb4448c48b
Robotz89 <96810015+Robotz89@users.noreply.github.com>
2021-12-29 13:38:04 -0600

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "I am now up and running as Galaxy Counting!"
+    return "I am now up and running as a Counting Bot!"
 
 def run():
   app.run(host='0.0.0.0',port=8080)
